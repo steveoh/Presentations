fbe79905600e4fd53957ac897fc9e23468634802
steveoh <sgourley@utah.gov>
2020-09-14 12:38:02 -0600

    style: single quotes

--- 2020/DowntownCodingSLC/1-command-line-without-parameters/main.py
+++ 2020/DowntownCodingSLC/1-command-line-without-parameters/main.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 # * coding: utf8 *
-"""
+'''
 main.py - A script that extracts spatial data for an address
-"""
+'''
 
 #: pros
 #: - no documentation required, run the script and it does what you last left it to do
 #: cons
-#: - dependencies are not easily installable
+#: - hard coded values
 #: - difficult to share - api key is unique to user
+#: - dependencies are not easily installable
 
 from dotenv import load_dotenv
 import requests
@@ -18,10 +19,10 @@ import sys
 
 locale.setlocale(locale.LC_ALL, '')
 load_dotenv()
-KEY = getenv("AGRC_API_KEY")
+KEY = getenv('AGRC_API_KEY')
 
 request = requests.get(
-    "http://api.mapserv.utah.gov/api/v1/geocode/123 south main street/slc", timeout=5, params={'apikey': KEY}
+    'http://api.mapserv.utah.gov/api/v1/geocode/123 south main street/slc', timeout=5, params={'apikey': KEY}
 )
 
 response = request.json()
@@ -39,7 +40,7 @@ y = location['y']
 print(f'address is at {x}, {y}')
 
 request = requests.get(
-    "http://api.mapserv.utah.gov/api/v1/search/sgid10.boundaries.counties/name",
+    'http://api.mapserv.utah.gov/api/v1/search/sgid10.boundaries.counties/name',
     timeout=5,
     params={
         'apikey': KEY,
@@ -60,7 +61,7 @@ name = result['attributes']['name']
 print(f'address is in {name} county')
 
 request = requests.get(
-    "http://api.mapserv.utah.gov/api/v1/search/sgid10.cadastre.landownership/owner",
+    'http://api.mapserv.utah.gov/api/v1/search/sgid10.cadastre.landownership/owner',
     timeout=5,
     params={
         'apikey': KEY,
@@ -81,7 +82,7 @@ owner = result['attributes']['owner']
 print(f'the land is owned by a {owner} entity')
 
 request = requests.get(
-    "http://api.mapserv.utah.gov/api/v1/search/sgid10.cadastre.parcels_saltlake_lir/total_mkt_value,bldg_sqft,floors_cnt,built_yr",
+    'http://api.mapserv.utah.gov/api/v1/search/sgid10.cadastre.parcels_saltlake_lir/total_mkt_value,bldg_sqft,floors_cnt,built_yr',
     timeout=5,
     params={
         'apikey': KEY,
@@ -107,7 +108,7 @@ print(
 )
 
 request = requests.get(
-    "http://api.mapserv.utah.gov/api/v1/search/sgid10.raster.usgs_dem_10meter/feet",
+    'http://api.mapserv.utah.gov/api/v1/search/sgid10.raster.usgs_dem_10meter/feet',
     timeout=5,
     params={
         'apikey': KEY,
